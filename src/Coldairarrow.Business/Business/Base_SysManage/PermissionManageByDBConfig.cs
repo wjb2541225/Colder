1cd8696dd8ab63b22db7e4798878c3a2305b8599
wjbo <wjb2541225@163.com>
2019-12-30 18:01:55 +0800

    1

---
+++ src/Coldairarrow.Business/Business/Base_SysManage/PermissionManageByDBConfig.cs
@@ -0,0 +1,70 @@
+﻿using Coldairarrow.Business.Base_SysManage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coldairarrow.Business.Business.Base_SysManage
+{
+    public class PermissionManageByDBConfig : IPermissionManage
+    {
+        public void ClearUserPermissionCache()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetAllPermissionValues()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PermissionModule> GetAppIdPermissionModules(string appId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetAppIdPermissionValues(string appId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetOperatorPermissionValues()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PermissionModule> GetRolePermissionModules(string roleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PermissionModule> GetUserPermissionModules(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetUserPermissionValues(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool OperatorHasPermissionValue(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetAppIdPermission(string appId, List<string> permissions)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetUserPermission(string userId, List<string> permissions)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUserPermissionCache(string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
